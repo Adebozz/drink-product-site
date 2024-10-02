1869b062b0140d043937b6df1c2c5fb9a4ab23c1
Adeboss <ademolaadeigbe3@gmail.com>
2024-10-02 18:01:06 +0100

    three scene

---
+++ src/components/ThreeScene.jsx
@@ -0,0 +1,34 @@
+// src/components/ThreeScene.jsx
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three";
+
+const RotatingBottle = () => {
+  const bottleRef = useRef();
+  const texture = useTexture("/assets/drink-label.jpg");
+
+  useFrame(() => {
+    bottleRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={bottleRef} position={[0, 0, 0]}>
+      <cylinderGeometry args={[1, 1, 4, 32]} />
+      <meshStandardMaterial map={texture} />
+    </mesh>
+  );
+};
+
+const ThreeScene = () => {
+  return (
+    <Canvas style={{ height: "100vh", background: "#ffdf00" }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <RotatingBottle />
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+  );
+};
+
+export default ThreeScene;
