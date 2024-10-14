a3be3abc0c0b5d3c2df9870b5e06d586b8ac8881
Adeboss <ademolaadeigbe3@gmail.com>
2024-10-14 22:02:37 +0100

    populated the database

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,13 @@
 import drink from '../assets/drink-label.jpg';
 import tropical from '../assets/tropical-juice-texture.jpg'
 import caprison from '../assets/th.jpg'
+import cocktail from '../assets/cocktail.jpg'
+import mocktail from '../assets/mocktail.jpg'
 
 export default {
     drink,
     tropical,
     caprison,
+    cocktail,
+    mocktail,
 };
