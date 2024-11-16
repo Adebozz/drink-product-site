98c0a2bfd88c03708ad1186fc44d0f567798eacb
Adeboss <ademolaadeigbe3@gmail.com>
2024-11-16 23:09:50 +0100

    set

--- backend/products.js
+++ backend/products.js
@@ -13,26 +13,6 @@ module.exports = [
     thumbnail: assets.drink,
     texture: assets.drink,
   },
-  {
-    id: 3,
-    name: "Caprison",
-    thumbnail: assets.caprison,
-    texture: assets.caprison,
-  },
-  {
-    id:4,
-    name: "mocktail",
-    thumbnail: assets.mocktail,
-    texture: assets.mocktail,
-    
-  },
-  {
-    id:5,
-    name: "cocktail",
-    thumbnail: assets.cocktail,
-    texture: assets.cocktail,
-    
-  },
   
   // Add more products as needed
 ];
